a8e142f6a26fefeecfafbcce1eda2843ab0a8d2c
Yaseen Davids <yaseendavids477@gmail.com>
2019-07-29 16:18:50 +0200

    Changed connection string

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,15 @@
 // UPDATE THIS WITH .env
-const username = 'admin',
-  password = 'admin',
+const
+  host = "ec2-54-235-92-43.compute-1.amazonaws.com",
+  username = 'xkidctbtiyklqn',
+  password = '0a25c3c4a2f6f0b3509726d6b413096e4019a39506988199f5d793e13847e791',
   port = '5432',
-  db = 'todownload';
+  db = 'd3m7eoc8rili7g';
 
 module.exports = {
   development: {
     client: 'pg',
-    connection: `postgres://${username}:${password}@localhost:${port}/${db}`,
+    connection: `postgres://${username}:${password}@${host}:${port}/${db}?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory`,
     migrations: {
       directory: __dirname + '/migrations',
     },
