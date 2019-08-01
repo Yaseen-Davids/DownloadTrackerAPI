eccc7ad1d99a2de6aff5780321a8bdcb01ac77b1
Yaseen-Davids <yasendavids477@gmail.com>
2019-08-01 11:59:37 +0200

    migrations removed

--- migrations/20190708081836_series_movies_todownlaod.js
+++ migrations/20190708081836_series_movies_todownlaod.js
@@ -1,15 +1,17 @@
 exports.up = async (knex) => {
   return knex.schema.createTable("series", (table) => {
-      table.increments("id").primary()
-      table.string("title")
-      table.float("season")
-      table.float("episode")
+      table.increments("id").primary();
+      table.string("title").notNullable().unique();
+      table.float("season").notNullable();
+      table.float("episode").notNullable();
       table.timestamp("created_at").defaultTo(knex.fn.now())
+      table.timestamp("updated_at").defaultTo(knex.fn.now())
     })
     .createTable("movies", (table) => {
-      table.increments("id")
-      table.string("title")
+      table.increments("id").primary();
+      table.string("title").notNullable().unique();
       table.timestamp("created_at").defaultTo(knex.fn.now())
+      table.timestamp("updated_at").defaultTo(knex.fn.now())
     })
 };
 
--- migrations/20190801091455_add_title_unique.js
+++
@@ -1,9 +0,0 @@
-exports.up = async (knex) => {
-  knex.schema.alterTable("series", function (table) {
-    table.string("title").unique().alter()
-  })
-};
-
-exports.down = async (knex) => {
-  // knex.schema.dropTableIfExists("series")
-};
